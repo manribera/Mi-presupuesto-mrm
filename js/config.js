a73b9f64ff52e9b8500f68ace0d596be9fdd3e44
manribera <manfredrivera.m@gmail.com>
2026-08-27 12:28:31 -0600

    Update API_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwj3gUrdEgoviCw2MPaxgNPg0ZYYg_aDWE_LfcUNxaXsHCBejiAxg-f4yer7iAISuY/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbyM5MYDbxRK_nFLRGemCJI4ISFokW4qEHqvMTfF7z3Uv9dzxQ8pD6pAfGa6a6gQIC32/exec"
 };
