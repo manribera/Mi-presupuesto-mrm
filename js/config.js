19e08d3e24e83bb7d5703432fb8b1b76b11c422b
manribera <manfredrivera.m@gmail.com>
2026-08-27 12:18:57 -0600

    Refactor CONFIG to use const and update API_URL

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
-window.CONFIG = {
-  // Pegue aquí la URL terminada en /exec que obtiene al implementar Code.gs.
-  API_URL: "PEGAR_AQUI_URL_DE_APPS_SCRIPT"
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbymb9ns-aHYBP-ptR2utqEG-w38kZcGsHYVBJX3hYg29Bhp_o43c6a3UYKTrb0xFxAa/exec"
 };
